d9ecbbfc538aa803e128d2747d79b882ba1f9829
Anastasiia Dashkova <dashkova.exceedteam@gmail.com>
2021-09-20 16:52:06 +0300

    fix rederect to /home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
 import MainStore from '../../stores/MainStore';
 import { PageHeader } from 'antd';
 import 'antd/dist/antd.css';
@@ -9,24 +9,29 @@ const Header = () => {
   const store = MainStore;
 
   const goToHomePage = () => {
-    history.push(`?page=1`);
+    history.push(`/?page=1`);
     store.page = 1;
     store.name = null;
     store.type = null;
+    store.isSearchByType = false;
     store.fetchWithDetails();
   };
 
   return (
-    <PageHeader
-      className="site-page-header"
-      style={{
-        backgroundColor: '#FFEF78',
-        marginBottom: '20px',
-      }}
-      title="Poké-library"
-      subTitle="From RESTful Pokémon API"
-      onBack={() => goToHomePage()}
-    />
+    <>
+      <Redirect to="/?page=1" />
+
+      <PageHeader
+        className="site-page-header"
+        style={{
+          backgroundColor: '#FFEF78',
+          marginBottom: '20px',
+        }}
+        title="Poké-library"
+        subTitle="From RESTful Pokémon API"
+        onBack={() => goToHomePage()}
+      />
+    </>
   );
 };
 
